601851e6ac59cd598cd51890ac3fca7239da6dae
banchuong <43121823+banchuong@users.noreply.github.com>
2018-12-13 07:08:24 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,48 @@
+<?php
+echo $red."[!] warning !!!".$t;
+echo $kuning."[-] ini adalah program ilegal".$t;
+echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+
+$msgtoto = "[!] subscribe channel sungging gtu 😆"."\n"."[!] atau chanel adi bordir guys"."\n"."[!] dijamin ori ";
+$msgyd = "[!] enjoy your life";
+$msgwe = "[!] info withdraw work with delay 10 sec";
+$msgveeu = "[!] enjoy your life";
+$data= [
+'adi bordir','Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
+'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
+'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
+'Asmuri Aas','Goril Aas','Wili Aas','Gery Aas',
+'Redi Aas','Fenjul','Sabar Ajah','Dewi Tati','Fenjul',
+'Hasan','Yuli Iyul','Yun Yuyun','Davis','Novita',
+'$ FARHAN Z $','Muhammad Nandars','Muh Nandar',
+'Dep Bell','Adis Ahmat','Wali Roy','Roy Purba',
+'Acik Mall','Salim Aas','Bela Aas','Andrie',
+'Andriean','Andriesun','Acik Mall','Salim Aas',
+'Bela Aas','Rujana Bakong','Farid Mohamed','Dewi Aas',
+'Dila Aas','Bili Aas','Flybird Yuan','Teri Aas',
+'Den Aas','Amin Aas','Wulan Aas','Hasan Aas','Yusri','Fachrul Ejp','ajit vepal','Agus Riyadi',
+'Agus Riyadi Riyadi','Richard Steven Try',
+'Yoviar Pauzi','Ayesha Advocate',
+//vipm
+'HARIS Denzlot','Raihan Chainago','Bejo Jo206','Bejo Rusak','Jordan Donathan','Bejo Mace','Jojo Vake','Deaso Bejo','Lungkik Cuiru','Riyan Kartika','Lusto Rusak','Jorsn Rusak','Jordwun jerdin','Curit Astjx','Hais Suis',
+//mpiv
+'Gem Bel','Hati Yang Terluka','Umi Kholiifah',
+'Aditiya Sarofi Putra','NOOB GAMING','phim lẻ','Jordwun lrus','gan bro','Gan Bro','bot siji',
+'bot loro','bot telu','yu di','aryant259','Janandito W',
+'arifin','Noob Player','yoyomamam90@gmail.com','Andi Aas','elang aja','Muhammad Paijo','Sutejo','Siti Azizah','Dian Kartika','Budi Aas','Raby Aas',
+'Wendy Aas','Rani Aas','Lê Hoàng','Rere Aas','Rony Aas',
+'Riki Aas','Junaidi Abdulah','Tidak Dikenal','Agung Nugraha','SobLok DanSka VoltHz','Afil X-friends Discography Hitam-Putih','Aan TheRG','Davin Hardit',
+'robert simangunsong','Restu OnGame','santa lidwyna tarigan','NEWBIE BERKELAS','Bambang Setiawan',
+'Mr. Benk','Yafia Ah','Yafiah Tri Ramdani wijaya',
+'nayz stone','Myvi Com','Seda Coeg','Wayan','Kiki Albani','Kresna Rizkie','itel meh','gretonger Tool','bball','Remsha Rajpout','Khalifi','Hasan Ahmmad YK','Ahmad_alfian','bondex pay','Mona Xperia',
+'BangGenzzot Gaming','Addinansah Azmil Hidayat','Elza Mila','astrajingga18','Astrajingga','Chiko Andrea',
+'Thoriq','Agus Saepuloh','Muhammad Feb','SWITCHCASE CHANNEL','djindonesia34@gmail.com','Hole Jewelry','Fina','Pooja Lalwani','Mobi legend','Agung Prakoso','manol watu','Nandi Wardana','Golek Duwit','kintel gedi','Haris Setyawan',
+'switchcase advertising','mody prasetya','Hasan Bgp','Reno Aas','Rexy Aas','Boby Aas','Reno Aas','Reva Aas','Riky Aas','Reff Aas','Rix Aas','Rahmy Aas','Bay Aas','Caca Aas','Yuki Aas','Rini Aas','Gege Aas','Adi Aas','Wewe Aas','Golek Duwit','Dapi Aas','Sapi Aas','Rehu Aas','Rehi Aas','Witi Aas','Akbar Cara','Akbar Oite','Akbar Akb','Akbar Albert','Loge Cara','Alis Cara','Rema Cara','Forta Cara','Fewe Cara',
+'Wani Aas','Rara Aas','Fewe Cara','arif','Rizal Rapz','Woodpecker Woody','Febryansyah','Babang Ary','Mila Milenia','Ary Ary','Jon Son','Tukafi Acucre',
+'Josie','Davita Brietta','Charisse Cariotta','Bilkis','Sundari','Reksi Aas','Adex Aas','Iyul Aas','Rexy Aas','Tery Aas','Ulex Aas','Sule Aas','Rex Aas','Fiikhozii JR Fiikhozii JR','Danu Antwo','ibnuramadhan','Ahmad Fathoni','Jeff Buizon','ayr official','Febryan','Febryan','Ika Sinta','Ujenk The Restovers','NoobSec ID','Berita Terkini','Paypal Hunter','Curhat Mas','kobe sacac','Yakub Setiawan','Zal Case',
+ 'Sofhia Harlan','Januar Krist','Benac','akmal miftah','Ainur Rofik','Gatanzi','Makarti','rafif dwi','Muhammad Dim','Aris Ryan','Karin Dyah','Haris Reno','Riri Melati','Lee Huy','Andi Setia','Afifah Dwi','Yuni Sara','Lisa Gea','Rangga Saputra','Sutrisno Abdi','Putra Maulana','SKIPPER FAME','Erik Irawan','ZiBn','Marcel Roni','Sherly Cantika','Suci Lestari','Novri Haris','Rico Pangabean','shandhe kalla','ragad jaya',
+'inpinix inpo','RONY Taylor','Farel Pratama','Azzahra Diana','Calista Arsy','Revano Aditya','Monic Ika','Gibran Alvin','Ilyasa Syauqy Hummam','Lukman Hakim','ilyasa syauqy','adham.farros22@gmail.com','Mustofa Aji','layla.axis125@gmail.com','zilonggg124@gmail.com','fanny.victory22@gmail.com','gusion.dagger121@gmail.com','lance.hitz120@gmail.com','lolita.axis23@gmail.com','balmon.botak19@gmail.com','lesley.lele99@gmail.com','lapulupa22@gmail.com',
+'triệu linh','Giew Giew','Jancog','Ok Oc','wayan dhanayasa','E27692',
+        ];
+$ydnews = ['tes',];
+$toto = ['tes',];
