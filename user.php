e43b9ed78c3674f79bbd594d13436c8edd15f7ba
banchuong <43121823+banchuong@users.noreply.github.com>
2019-01-18 22:46:47 +0700

    Add files via upload

--- user.php
+++ user.php
@@ -2,17 +2,83 @@
 echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+$stat_ads=true;
+$sungging = $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'I46346';
+$ads= $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'I46346'.$t.$putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
 
-$msgtoto = "[!] subscribe channel sungging gtu 😆"."\n"."[!] atau chanel adi bordir guys"."\n"."[!] dijamin ori ";
-$msgyd = "[!] enjoy your life";
-$msgwe = "[!] info withdraw work with delay 10 sec";
+$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$ijo."[•••••] sekilas info [••••••]
+
+hallo guys apa kabar
+yang masih setia menunggu update script terbaru toto
+sedikit sy jelaskan perjalanan kita
+>> toto berganti menjadi TTNews 
+>> TTNews berganti menjadi watch news
+>> watch news berganti menjadi cworld
+
+nah saat ini cworld masih dalam tahap beta tes
+untuk yg blm punya apk cworld bisa di download
+
+di sini >> https://sfile.mobi/8ZPF2b181Ut
+
+atau kunjungi playstore
+di sini >> http://news.masjmzs.com/index/openredpacket?unique=e1d4bac1b720a2af7c0acf3d8f81617c&d=31&lng=in
+ 
+dan disini >> 
+https://play.google.com/store/apps/details?id=com.news.newsyayd
+
+setelah menginstall aplikasi terbaru cworld
+silahkan login menggunakan akun favorit kalian
+dan ketika di minta pilihan
+ >> new user atau old user <<
+silahkan pilih new user untuk menghilangkan jejak kita
+setelah login berhasil masuk ke tab profil
+di kanan bawah dan pergi untuk masukan kode undangan
+silahkan memasukan kode >> A15280 <<
+
+skrip akan sy update setelah close beta tes
+atau setelah cworld di rillis
+jika sebelum rillis ada 200+ user
+menggunakan kode saya maka skrip akan sy buat free
+tapi jika kurang dari itu
+maka system validasi akan berlaku seperti watch earn
+
+demikian sekilas info$putih
+msg_by:$ijo adidoank";
+
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["Imam Almasyah Risanzani",];
+$ydblock = ["Imam Almasyah Risanzani",];
+$we_stat = true;
+$msgwe = "[•] update vip versi 2.0 ";
 $msgveeu = "[!] enjoy your life";
-$data= [
-'adi bordir',
+
+$data= ['trieulinhlin','Hung Cao','Linh Linh','Uyên Phương','Chương Bàn','Uyên','Linh',
 //vipm
-'trieulinhlin','Hung Cao','Linh Linh','Uyên Phương','Chương Bàn','Uyên','Linh',
+
 //mpiv
-'name',
-        ];
-$ydnews = ['tes',];
+
+/*
+ 'N58140','O10784','J15232','V27584','M94778','V98855','H44906','U61395',
+ */
+ 
+//vip
+"W96630",
+//adidonk 
+'serenamonic1','Adam Ersanur','Wind Ci','TANPA BATAS','Alifian Dzaki','LK tv','ilham maulana','Duki Duketo','Danang Mahmud','Alto Hadi Lukita Jaya','Adam Ersanur',
+'Putry We','Mhmd Jejen','ssophan channel','Ferdy Ndest','Nanang Hermansyah','Dede Mukhtar','Lastlah Anjing','Fauji Ikhwana','Red Roseate','Red Roseate','faisal ztm','mobile lengendary',
+//sungging
+'Safitri ,','ANAK TUTORIAL','Azis Herjuno','Eka Jaya','Aji Maulana','Klasynky Gege','DEni Muach','Mustofa Agil','Viki Waluyo','TMCyber ArmyTM','Ferdy Rizaldi','Anun YK Ina','Len','Bilal Maulana','Maman','Angga Sena Casandra','paulsculd678@gmail.com','ANGGI','Muhammad Ikbal','Jrock Star','Muhammad Irman Raya','Purwono','Ikbal Kepalasuku Ujungpanrang','baca plus','Lucky Lucky','Zedrik Bam','Rahma Only\'For One\'Heart','Muhammad Zafran Alfariq','Agara Net',
+//input
+ 'APRI YANTO','Aditya Ramad','Anun Dwi Yulianto',
+'Veranda vee','Miyako Titin','Gugung Jagi','G12717','Sinyorie Enorie','Ventura','Sry Jayaa','Kontoru Sama','Kwarasan Kenapa','Fanss Milyha','game gue','Cemul','Milyha Dipa','Parzy Leor','Alifian Dzaki','Meka Alfin Permana','Tukang Copas','adieb nur hafidz','Milytha Pabji','Ridhos Hago','Ridho YK','Nogotirto YK','tuyul baru1','Thursina Anna','Dja Rot','Dini Suswanti','宙宜尺尹','Krisna Ilham','Yogi Martha','Kristiana Honnie pilsa','N\'chev Jepank','asepairil22@gmail.com','Muh Zuhdi','Kang Roki','Fachrurodzii','Moch Tham','Dìmoka Deo','Faris HariSandy','Muh Zuhdi','Rizaldi Alfian','Roselle Santos','yusufsapta31@gmail.com','Tajul Arifin','Lesley El','Angel Putri','fara ciprit10','Maniac Receh','Alana Basil','EL Frank Ethan','ksupri541@gmail.com','mawar150690@gmail.com','Salsabila','Acek Wew','Kokyu Misariu','Dejon Raon','Joko Sutopo','Barep Pras W',                       'Galuh Prayoga','ramdhan achmad','Aji Saputra Ramadan','Hendra','Dany Catur','EJ','Setya Rangga','Arief Poenya','Ardy LoveHeaven Nabilla','Adi Samsul','Clay Dark','egi pangestu','凡Ɗιs ANDEZ','RANGGEN I-on','joni joni',
+'Ahmad Syarifudin','Anime Indo','Vipsaw','Heels Ru','Mardha Waty','andi arjuna','GAREL Hent.','Tian Sopyan','Yanuar Fernandez','Afin Bachtiar','Minu Suherman','King Bug','Dwi Putra','BAHNDITE','Gelas Akua','Febri Hamunangan','Dhanka Maaikha','maru dressden','miftakhul arif','Rahmat Julianto','prayoga galuh','Alfian Tamran',
+'Berog Gan','sa ngak','samplokndasmu4@gmail.com','barep praset','Gillang Ellyntina','afara har','Gilang 973','Zackriyya Wong','Curang DROID','Ardy Van Houten','Ahmad Fallz','Mario Teguh','TERBIT SANG SURYA','Dede Mukhtar','Adhy Samsul',
+'Rodenmaa R','Jajang Asmara','love Bitcoin','Dhiksa Iswara','song wai','Manuk Piit','Ano Nimus','Khaira Sahid','My Son','shinigami','Ogita Rumansyah','AS Gaming','Aiko Kenangan','Fata Habiburrohman','Adeel Chand',
+'LEN. ID','All Game Online27','ffdin dpn','Dani Donk1','Aiola Leo','gear second','alvaro putra','Syamsul Amir','Angela Maras','Dhe Puttri','masasep hasa','Yaah Saaa','Little Night','Fauzistiawan Putra Negerikubang','Arif',
+'fara ciprit01','Nicholas Parasian Tampubolon','Rhavael Sumilat','Suko Pauris','Nicholas Parasian Tampubolon','Joko Tingkir','Adi Loro','Angela Maras','Random ChannelTM','GM Kardus','Lukman Hakim',
+ ];
+
 $toto = ['tes',];
+$ydnews = ['tes','adidoank',];
